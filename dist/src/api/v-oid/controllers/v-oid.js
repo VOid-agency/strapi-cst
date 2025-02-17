2c258298ff04682d70a7a305ce98965deaf86541
bruno.xavier <bruno.xavier@multivis.com>
2025-02-17 18:53:54 -0300

    fix: add dist

---
+++ dist/src/api/v-oid/controllers/v-oid.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * v-oid controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::v-oid.v-oid');
---
+++ dist/src/api/v-oid/services/v-oid.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * v-oid service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::v-oid.v-oid');
