21c5c65cf00a9baed8590c3720a2cd755681cfc8
Void Agency <admin@voidagency.com.br>
2025-02-17 19:22:33 -0300

    Delete dist directory

--- dist/src/api/v-oid/routes/v-oid.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * v-oid router
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::v-oid.v-oid');
